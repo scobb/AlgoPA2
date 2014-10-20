2e2c308fe3f5e465b9a7051309a1072c63af8781
Steve Cobb <scobb1@utexas.edu>
2014-10-19 22:49:29 -0500

    Updating test to be a little more robust

--- test.sh
+++ test.sh
@@ -1,12 +1,22 @@
+# script requires RUN.py is in the same directory, and that sample ins/outs are in
+# <LOCAL_DIR>/sample_tests
+if [ ! -d test ]; then
+    mkdir test 
+fi
 regex="in([0-9]+)\.txt"
 for file in $(ls sample_tests)
 do
     if [[ $file =~ $regex ]]; then
         num="${BASH_REMATCH[1]}"
 	./RUN.py sample_tests/$file > test/out$num
-        OUTPUT=$OUTPUT"$(diff sample_tests/out$num.txt test/out$num)"
+        mydiff=$(diff sample_tests/out$num.txt test/out$num)
+        if [[ -n $mydiff ]]; then
+            OUTPUT=$OUTPUT"Test $num failed: $mydiff"
+        fi
     fi
 done
 if [[ -z $OUTPUT ]]; then
     echo PASS
+else
+    echo $OUTPUT
 fi
